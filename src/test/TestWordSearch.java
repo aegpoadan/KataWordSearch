36ecaffe527b97824c96bb798b01927c9595b54d
aegpoadan <ahayes2425@gmail.com>
2018-02-16 01:19:36 -0500

    Added finding horizontal words

--- src/test/TestWordSearch.java
+++ src/test/TestWordSearch.java
@@ -1,42 +1,75 @@
 package test;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 import wordSearch.WordSearch;
 
 public class TestWordSearch {
-	private static final String firstTestFile = "./WordSearch1.txt";
-	private static final String firstWordsToSearchForString = "BONES,KHAN,KIRK,SCOTTY,SPOCK,SULU,UHURA";
-	private static final String firstBoggleBoardString = "U,M,K,H,U,L,K,I,N,V,J,O,C,W,E\n"
-			+ "L,L,S,H,K,Z,Z,W,Z,C,G,J,U,Y,G\n"
-			+ "H,S,U,P,J,P,R,J,D,H,S,B,X,T,G\n"
-			+ "B,R,J,S,O,E,Q,E,T,I,K,K,G,L,E\n"
-			+ "A,Y,O,A,G,C,I,R,D,Q,H,R,T,C,D\n"
-			+ "S,C,O,T,T,Y,K,Z,R,E,P,P,X,P,F\n"
-			+ "B,L,Q,S,L,N,E,E,E,V,U,L,F,M,Z\n"
-			+ "O,K,R,I,K,A,M,M,R,M,F,B,A,P,P\n"
-			+ "N,U,I,I,Y,H,Q,M,E,M,Q,R,Y,F,S\n"
-			+ "E,Y,Z,Y,G,K,Q,J,P,C,Q,W,Y,A,K\n"
-			+ "S,J,F,Z,M,Q,I,B,D,B,E,M,K,W,D\n"
-			+ "T,G,L,B,H,C,B,E,C,H,T,O,Y,I,K\n"
-			+ "O,J,Y,E,U,L,N,C,C,L,Y,B,Z,U,H\n"
-			+ "W,Z,M,I,S,U,K,U,R,B,I,D,U,X,S\n"
-			+ "K,Y,L,B,Q,Q,P,M,D,F,C,K,E,A,B";
+	private WordSearch wordSearchTest;
+	private String searchResult;
+	
+	public TestWordSearch() throws IOException {
+		wordSearchTest = new WordSearch(TestWordSearchStrings.firstTestFile);
+		searchResult = wordSearchTest.findWordsToSearchFor();
+	}
 	
 	@Test
 	public void testConstructionAndBoggleBoardPrint() {
-		WordSearch test = new WordSearch(firstTestFile);
-		assert(test.getBoggleBoardString().equals(firstBoggleBoardString));
+		assert(wordSearchTest.getBoggleBoardString().equals(TestWordSearchStrings.firstBoggleBoardString));
 	}
 	
 	@Test
 	public void testConstructionAndWordsToSearchForPrint() {
-		WordSearch test = new WordSearch(firstTestFile);
-		assert(test.getWordsToSearchForString().equals(firstWordsToSearchForString));
+		assert(wordSearchTest.getWordsToSearchForString().equals(TestWordSearchStrings.firstWordsToSearchForString));
 	}
-
-	public static void main(String[] args) {
-		new TestWordSearch().testConstructionAndBoggleBoardPrint();
-		new TestWordSearch().testConstructionAndWordsToSearchForPrint();
+	
+	@Test
+	public void testFindWordHorizontal() {
+		String searchResult = wordSearchTest.findWord("SCOTTY");
+		assert(searchResult.equals(TestWordSearchStrings.firstHorizontalCorrectResult));
 	}
+	
+	/*
+	@Test
+	public void testFindWordsToSearchForVertical() {
+		
+	}
+	
+	@Test
+	public void testFindWordsToSearchForDiagonalAscending() {
+		
+	}
+	
+	@Test
+	public void testFindWordsToSearchForDiagonalDescending() {
+		
+	}
+	
+	@Test
+	public void testFindWordsToSearchForHorizontalReverse() {
+		
+	}
+	
+	@Test
+	public void testFindWordsToSearchForVerticalReverse() {
+		
+	}
+	
+	@Test
+	public void testFindWordsToSearchForDiagonalAscendingReverse() {
+		
+	}
+	
+	@Test
+	public void testFindWordsToSearchForDiagonalDescendingReverse() {
+		
+	}
+	
+	@Test
+	public void testFindWordsToSearchFor() {
+		assert(wordSearchTest.getWordsToSearchForString().equals(TestWordSearchStrings.firstCorrectResult));
+	}
+	*/
 }
--- src/wordSearch/WordSearch.java
+++ src/wordSearch/WordSearch.java
@@ -4,25 +4,35 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
-import java.util.Map;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
-import org.apache.commons.collections4.bidimap.DualHashBidiMap;
+import org.apache.commons.lang3.tuple.Pair;
 
 public class WordSearch {
 	private ArrayList<ArrayList<Character>> boggleBoard = new ArrayList<ArrayList<Character>>();
 	private ArrayList<String> wordsToSearchFor = new ArrayList<String>();
 	//first int in entry is x coordinate, second is y coordinate
-	private DualHashBidiMap<String, Map.Entry<Integer, Integer>[]> wordMap = new DualHashBidiMap<String, Map.Entry<Integer, Integer>[]>();
+	private HashMap<String, ArrayList<Pair<Integer, Integer>>> wordMap = new HashMap<String, ArrayList<Pair<Integer, Integer>>>();
+	private int numColumns;
+	private int numRows;
 	
-	public WordSearch(String path) {
+	public WordSearch(String path) throws IOException {
 		try {
 			String fileContent = new String(Files.readAllBytes(Paths.get(path)), Charset.defaultCharset());
 			String[] lines = fileContent.split("\n");
+			numRows = lines.length - 1;
+			numColumns = numRows;
 			int i = 0;
 			
 			for(String line: lines) {
 				String[] words = line.split(",");
+				if(i > 0 && words.length != numColumns) {
+					throw new IOException("WordSearch matrix is incorrectly formatted! Must be NxN");
+				}
 				
 				if(i == 0) {
 					for(String word: words) {
@@ -38,9 +48,63 @@ public class WordSearch {
 				
 				i++;
 			}
+			
+			loadWordMap();
 		} catch(IOException E) {
 			System.err.println("Failed to read file at path: " + path);
 		}
+	}
+	
+	private void loadWordMap() {
+		for(int i=0; i<numRows; i++) {
+			for(int j=0; j<numColumns; j++) {
+				getHorionztalWords(new LinkedHashSet<SimpleEntry<String, ArrayList<Pair<Integer, Integer>>>>(), 
+						new StringBuilder(), i, j);
+			}
+		}
+	}
+	
+	private Set<SimpleEntry<String, ArrayList<Pair<Integer, Integer>>>> 
+		getHorionztalWords(LinkedHashSet<SimpleEntry<String, ArrayList<Pair<Integer, Integer>>>> currentSet, 
+				StringBuilder currentWord, int x, int y) {
+		if(x >= numColumns) {
+			return currentSet;
+		}
+
+		addEntryToMap(currentSet, currentWord, x, y);
+		
+		return getHorionztalWords(currentSet, currentWord, x+1, y);
+	}
+	
+	private void addEntryToMap(LinkedHashSet<SimpleEntry<String, ArrayList<Pair<Integer, Integer>>>> currentSet, 
+			StringBuilder currentWord, int x, int y) {
+		SimpleEntry<String, ArrayList<Pair<Integer, Integer>>> lastEntry = (currentSet.size() > 0) ? 
+				currentSet.stream().skip(currentSet.size()-1).findFirst().get() : 
+				null;
+		
+		Character currentChar = boggleBoard.get(y).get(x);
+		currentWord.append(currentChar);
+		String key = new String(currentWord.toString());
+		
+		ArrayList<Pair<Integer, Integer>> value = (currentSet.size() > 0) ? 
+				new ArrayList<Pair<Integer, Integer>>(lastEntry.getValue()) : 
+				new ArrayList<Pair<Integer, Integer>>();
+		value.add(Pair.of(x, y));
+		
+		currentSet.add(new SimpleEntry<String, ArrayList<Pair<Integer, Integer>>>(key, value));
+		wordMap.put(key, value);
+	}
+	
+	public String findWordsToSearchFor() {
+		StringBuilder result = new StringBuilder();
+		
+		for(String word: wordsToSearchFor) {
+			result.append(findWord(word));
+			result.append("\n");
+		}
+		result.deleteCharAt(result.length() - 1);
+		
+		return result.toString();
 	}
 	
 	public String getBoggleBoardString() {
@@ -65,6 +129,21 @@ public class WordSearch {
 			result.append(word + ",");
 		}
 		result.deleteCharAt(result.length() - 1);
+		
+		return result.toString();
+	}
+	
+	public String findWord(String word) {
+		StringBuilder result = new StringBuilder();
+		
+		ArrayList<Pair<Integer, Integer>> coords = wordMap.get(word);
+		if(coords != null) {
+			result.append(word + ": ");
+			for(Pair<Integer, Integer> p: coords) {
+				result.append("(" + p.getKey() + "," + p.getValue() + "),");
+			}
+			result.deleteCharAt(result.length() - 1);
+		}
 		
 		return result.toString();
 	}
